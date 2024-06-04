d4423317426924b0328a7a87650cac1bf4fc8ef4
Kyrylo <kyryloyunytsky@gmail.com>
2024-06-04 06:07:48 +0300

    allow cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors());
 app.use(cookieParser());
 
 // Routes
